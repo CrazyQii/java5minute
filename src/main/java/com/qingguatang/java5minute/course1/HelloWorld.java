4591e6a828781521787dd551757fea27518b6ac2
CrazyQii <858590598@qq.com>
2019-05-29 13:45:30 +0800

    第一阶段
    创建Spring boot

--- src/main/java/com/qingguatang/java5minute/course1/HelloWorld.java
+++ src/main/java/com/qingguatang/java5minute/course1/HelloWorld.java
@@ -1,9 +1,14 @@
 package com.qingguatang.java5minute.course1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //注释类
 @RestController
 public class HelloWorld {
-    public void hello
+    @RequestMapping(value = "/")
+    public String hello() {
+        String message = "hello world";
+        return message;
+    }
 }
