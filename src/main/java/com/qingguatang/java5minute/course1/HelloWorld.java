f0107b52b24dee08110441696729663ef325b159
CrazyQii <858590598@qq.com>
2019-05-29 13:39:48 +0800

    Initial commit

---
+++ src/main/java/com/qingguatang/java5minute/course1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.qingguatang.java5minute.course1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//注释类
+@RestController
+public class HelloWorld {
+    public void hello
+}
